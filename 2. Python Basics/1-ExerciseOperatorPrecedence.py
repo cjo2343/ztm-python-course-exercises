70c84398ceea551cfffa61ee1f2b4486cda78537
cjo2343 <cjohn@itu.dk>
2025-08-30 23:24:38 +0200

    exercise done

--- 2. Python Basics/1-ExerciseOperatorPrecedence.py
+++ 2. Python Basics/1-ExerciseOperatorPrecedence.py
@@ -1,12 +1,14 @@
-# Guess the output of each answer before you click RUN
-# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
-print((5 + 4) * 10 / 2)
 
-print(((5 + 4) * 10) / 2)
+#Guess the output of each answer before you click RUN
+#Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
-print((5 + 4) * (10 / 2))
+print((5 + 4) * 10 / 2) # = print(9 * 5) = 45
 
-print(5 + (4 * 10) / 2)
+print(((5 + 4) * 10) / 2) # = print((9 * 10) / 2) = print(90 / 2) = print(45)
 
-print(5 + 4 * 10 // 2)
+print((5 + 4) * (10 / 2)) # = print((9) * (5)) = print(45)
+
+print(5 + (4 * 10) / 2)  # = print((5) + ((40) / 2)) = print((5) + 20) = print(25)
+
+print(5 + 4 * 10 // 2) # = print(5 + 4 * 5) = print(5 + 20) = print(20)
