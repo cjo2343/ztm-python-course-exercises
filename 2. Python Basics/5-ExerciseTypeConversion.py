5f617f58ada501457cca5bbbc332fdc62c6f0665
cjo2343 <cjohn@itu.dk>
2025-09-03 10:35:18 +0200

    exercises

--- 2. Python Basics/5-ExerciseTypeConversion.py
+++ 2. Python Basics/5-ExerciseTypeConversion.py
@@ -1,6 +1,20 @@
 # Exercise Type Conversion
 # Write a program which tells your age
 
-birth_year = input('What year were you born?\n')
-age = 2024 - int(birth_year)
-print(f'Your age is {age} years.')
+#birth_year = input('What year were you born?\n')
+#age = 2024 - int(birth_year)
+#print(f'Your age is {age} years.')
+
+
+
+#birth_year = input('What year were you born?')
+#this_year = 2025
+#actual_age = this_year - int(birth_year)
+#minimum_age = 18
+#allowed = False
+#print(f'If you were born in {birth_year}, then your age is: {actual_age} years old.')
+#if this_year - int(birth_year) > minimum_age 
+
+
+
+
--- 2. Python Basics/6-ExercisePasswordChecker.py
+++ 2. Python Basics/6-ExercisePasswordChecker.py
@@ -5,3 +5,14 @@ password = input('Enter you password:\t')
 
 secret_password = len(password) * '*'
 print(f'Hey {username}, your password {secret_password} is {len(password)}  letters long.')
+
+
+
+username = input('What is your username?')
+password = input('Write your password')
+
+password_length = len(password)
+secret_pw = '*' * password_length
+
+sentence = (f'Hello, {username}, your password {secret_pw} has been accepted. It is {password_length} characters long.')
+print(sentence)
